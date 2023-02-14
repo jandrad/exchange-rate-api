984b30b93fd8edd2e72bfe969e2331dcd13160ac
José Andrade <jandrad@gmail.com>
2023-02-14 10:31:27 +0100

    Support different tickers

---
+++ api/crypto/[ticker].ts
@@ -0,0 +1,38 @@
+import cors from '../../lib/cors'
+
+export const config = {
+    runtime: 'edge',
+};
+
+async function rate(symbol?: string | null): Promise<Response> {
+    try {
+        if (!symbol) {
+            return new Response('No symbol provided', { status: 400 });
+        }
+        const result = await fetch(`https://cryptoprices.cc/${symbol}`, {
+            method: 'GET',
+            redirect: 'follow',
+            headers: {
+                'Content-Type': 'text/plain',
+            }
+        });
+        const rate = await result.text();
+        if (!rate) {
+            return new Response('No rate found', { status: 404 });
+        }
+        return new Response(rate, { headers: { 'Cache-Control': 's-maxage=600' } });
+    } catch (error) {
+        return new Response(error.message, { status: 500 });
+    }
+}
+
+export default async (req: Request) => {
+    let res = new Response("");
+    if (req.method === 'GET') {
+        const url = new URL(req.url);
+        const urlSearchParams = new URLSearchParams(url.search);
+        const ticker = urlSearchParams.get('ticker')?.toUpperCase();
+        res = await rate(ticker);
+    }
+    return cors(req, res);
+};
--- api/fiat/[ticker].ts
+++ api/fiat/[ticker].ts
@@ -4,10 +4,13 @@ export const config = {
     runtime: 'edge',
 };
 
-async function rate(symbol: string): Promise<Response> {
+async function rate(symbol?: string | null): Promise<Response> {
     try {
+        if (!symbol) {
+            return new Response('No symbol provided', { status: 400 });
+        }
         const apikey = process.env.API_KEY || '';
-        const result = await fetch(`https://api.currencyfreaks.com/latest?apikey=${apikey}&symbols=COP`, {
+        const result = await fetch(`https://api.currencyfreaks.com/latest?apikey=${apikey}&symbols=${symbol}`, {
             method: 'GET',
             redirect: 'follow',
             headers: {
@@ -16,7 +19,7 @@ async function rate(symbol: string): Promise<Response> {
             }
         });
         const data = await result.json();
-        const rate = data?.rates?.['COP'];
+        const rate = data?.rates?.[symbol];
         if (!rate) {
             return new Response('No rate found', { status: 404 });
         }
@@ -30,7 +33,10 @@ export default async (req: Request) => {
 
     let res = new Response("");
     if (req.method === 'GET') {
-        res = await rate('COP');
+        const url = new URL(req.url);
+        const urlSearchParams = new URLSearchParams(url.search);
+        const ticker = urlSearchParams.get('ticker')?.toUpperCase();
+        res = await rate(ticker);
     }
     return cors(req, res);
 };
