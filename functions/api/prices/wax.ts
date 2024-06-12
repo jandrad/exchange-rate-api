e61e4cc80690cd25c218ba839344c2da17815bb4
José Andrade <jandrad@gmail.com>
2024-06-12 16:37:08 +0200

    Add endpoint to get wax tokens prices

---
+++ functions/api/prices/wax.ts
@@ -0,0 +1,59 @@
+import cors from '../../../lib/cors'
+
+async function prices(): Promise<Response> {
+    try {
+        const [{ rows: waxRows }, woePrices] = await Promise.all([
+            fetch(`https://wax.neftyblocks.com/v1/chain/get_table_rows`, {
+                method: 'GET',
+                redirect: 'follow',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'request',
+                },
+                body: JSON.stringify({
+                    index_position: 3,
+                    key_type: "i64",
+                    code: "delphioracle",
+                    scope: "waxpusd",
+                    table: "datapoints",
+                    limit: 1,
+                    reverse: true,
+                    json: true,
+                    show_payer: false,
+                }),
+            }).then((r) => r.json()),
+            fetch(`https://mev-api.waxonedge.app/tokens`, {
+                method: 'GET',
+                redirect: 'follow',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'request',
+                }
+            }).then((r) => r.json()),
+        ]);
+
+        const prices: Record<string, number> = {};
+        const waxPrice = Math.round((waxRows[0].median / 10000) * Math.pow(10, 8)) / Math.pow(10, 8);
+        for (let i = 0; i < woePrices.length; i++) {
+            const price = woePrices[i];
+            const key = `${price.symbol.ticker}@${price.contract}`;
+            if (prices.wax_price) {
+                prices[key] = price.wax_price * waxPrice;
+            }
+        }
+
+        return new Response(JSON.stringify(prices), { headers: { 'content-type': 'application/json'}, });
+    } catch (error) {
+        if (error instanceof Error) {
+            return new Response(error.message, { status: 500 });
+        }
+        return new Response('Unknown error', { status: 500 });
+    }
+}
+
+
+
+export const onRequestGet: PagesFunction = async({ request, params }) => {
+    const res = await prices();
+    return cors(request, res);
+};
