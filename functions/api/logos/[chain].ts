971b861524bdc39732badf3f60ccc2192598b6c5
José Andrade <jandrad@gmail.com>
2024-06-21 12:50:18 +0200

    Separate logos cache

--- functions/api/logos/[chain].ts
+++ functions/api/logos/[chain].ts
@@ -2,6 +2,152 @@ import { config } from "../../../config";
 import { cors, useFetch } from "../../../lib";
 import { getURLParameters, timestamp, validTimestamp } from "../../../utils";
 
+async function getEosCafeLogos(env: KVNamespace, chain: string): Promise<Record<string, any>> {
+    const cacheId = `NEFTY_${chain.toUpperCase()}_LOGOS_EOSCAFE`;
+    const store = await env.get(cacheId);
+    const cached = store ? JSON.parse(store) : null;
+    if (cached && validTimestamp(cached.timestamp)) return cached.data;
+    const { data, error } = await useFetch<{ logo: string; logo_lg: string; symbol: string; account: string }[]>(
+        "/eoscafe/eos-airdrops/master/tokens.json",
+        {
+            baseUrl: config.GITHUB_FILES,
+            headers: {
+                "Content-Type": "application/json",
+                "User-Agent": "request",
+            },
+        }
+    );
+
+    if (error || !data) {
+        console.error(error);
+        return cached.data || {};
+    }
+
+    const logos: Record<string, any> = {};
+    const filterData = data.filter((token: any) => token.chain === chain);
+
+    for (let i = 0; i < filterData.length; i++) {
+        const token = filterData[i];
+
+        logos[`${token.symbol}@${token.account}`] = {
+            logo: token.logo,
+            logo_lg: token.logo_lg,
+        };
+    }
+
+    await env.put(
+        cacheId,
+        JSON.stringify({
+            // cache for 1 hour
+            timestamp: timestamp(3600),
+            data: logos,
+        })
+    );
+
+    return logos;
+}
+
+async function getAlcorLogos(env: KVNamespace, chain: string): Promise<Record<string, any>> {
+    const cacheId = `NEFTY_${chain.toUpperCase()}_LOGOS_ALCOR`;
+    const store = await env.get(cacheId);
+    const cached = store ? JSON.parse(store) : null;
+    if (cached && validTimestamp(cached.timestamp)) return cached.data;
+
+    const { data, error } = await useFetch<{ name: string; download_url: string }[]>(
+        `/repos/avral/alcor-ui/contents/assets/tokens/${chain.toLowerCase()}`,
+        {
+            baseUrl: config.GITHUB_API,
+            headers: {
+                "Content-Type": "application/json",
+                "User-Agent": "request",
+            },
+        }
+    );
+
+    if (error || !data) {
+        console.error(error);
+        return cached.data || {};
+    }
+
+    const logos: Record<string, any> = {};
+
+    for (let i = 0; i < data.length; i++) {
+        const { name, download_url } = data[i];
+
+        const nameWithoutLastExtension = name.split(".").slice(0, -1).join(".");
+        const [symbol, contract] = nameWithoutLastExtension.split("_");
+
+        if (symbol && contract) {
+            logos[`${symbol.toUpperCase()}@${contract.toLowerCase()}`] = {
+                logo: download_url,
+                logo_lg: download_url,
+            };
+        }
+    }
+
+    await env.put(
+        cacheId,
+        JSON.stringify({
+            // cache for 1 hour
+            timestamp: timestamp(3600),
+            data: logos,
+        })
+    );
+
+    return logos;
+}
+
+async function getLaunchbagzLogos(env: KVNamespace, chain: string, url?: string): Promise<Record<string, any>> {
+    if (!url) return {};
+
+    const cacheId = `NEFTY_${chain.toUpperCase()}_LOGOS_LAUNCHBAGZ`;
+    const store = await env.get(cacheId);
+    const cached = store ? JSON.parse(store) : null;
+    if (cached && validTimestamp(cached.timestamp)) return cached.data;
+
+    const { data, error } = await useFetch<{ data: { token_code: string; token_contract: string; image: string }[] }>(
+        "/launchbagz/v1/tokens",
+        {
+            baseUrl: url,
+            params: {
+                limit: Number(1000).toString(),
+            },
+            headers: {
+                "Content-Type": "application/json",
+                "User-Agent": "request",
+            },
+        }
+    );
+
+    if (error || !data?.data) {
+        console.error(error);
+        return cached.data || {};
+    }
+
+    const logos: Record<string, any> = {};
+    for (let i = 0; i < data.data.length; i++) {
+        const token = data.data[i];
+
+        if (token.image) {
+            logos[`${token.token_code.toUpperCase()}@${token.token_contract}`] = {
+                logo: `${config.NEFTY_IPFS}/ipfs/${token.image}`,
+                logo_lg: `${config.NEFTY_IPFS}/ipfs/${token.image}`,
+            };
+        }
+    }
+
+    await env.put(
+        cacheId,
+        JSON.stringify({
+            // cache for 1 hour
+            timestamp: timestamp(3600),
+            data: logos,
+        })
+    );
+
+    return logos;
+}
+
 async function tokens({
     env,
     token,
@@ -14,128 +160,30 @@ async function tokens({
     if (!chain) return new Response("No chain provided", { status: 400 });
 
     try {
-        const id = `NEFTY_${chain.toUpperCase()}_LOGOS`;
-        const store = await env.get(id);
-        const parsed = store ? JSON.parse(store) : null;
+        let mainChain = chain;
+        let launchbagzUrl = chain.includes("wax") ? config.NEFTY_API : undefined;
 
-        let logos: Record<string, any> = {};
+        if (chain.includes("test")) {
+            mainChain = chain.replace("testnet", "").replace("test", "");
 
-        if (parsed && validTimestamp(parsed.timestamp)) logos = parsed.data;
-        else {
-            let mainChain = chain;
-            let launchbagzUrl = chain.includes("wax") ? config.NEFTY_API : undefined;
-
-            if (chain.includes("test")) {
-                mainChain = chain.replace("testnet", "").replace("test", "");
-
-                if (launchbagzUrl) launchbagzUrl = config.NEFTY_API_TEST;
-            } else if (chain.includes("main")) {
-                mainChain = chain.replace("mainnet", "").replace("main", "");
-            }
-
-            const [eosCafePromise, alcorPromise, launchbagzTokens] = await Promise.allSettled([
-                useFetch("/eoscafe/eos-airdrops/master/tokens.json", {
-                    baseUrl: config.GITHUB_FILES,
-                    headers: {
-                        "Content-Type": "application/json",
-                        "User-Agent": "request",
-                    },
-                }),
-                useFetch(`/repos/avral/alcor-ui/contents/assets/tokens/${mainChain.toLowerCase()}`, {
-                    baseUrl: config.GITHUB_API,
-                    headers: {
-                        "Content-Type": "application/json",
-                        "User-Agent": "request",
-                    },
-                }),
-                launchbagzUrl
-                    ? useFetch("/launchbagz/v1/tokens", {
-                          baseUrl: launchbagzUrl,
-                          params: {
-                              limit: Number(1000).toString(),
-                          },
-                          headers: {
-                              "Content-Type": "application/json",
-                              "User-Agent": "request",
-                          },
-                      })
-                    : Promise.reject(new Error("No launchbagz url provided")),
-            ]);
-
-            if (
-                alcorPromise.status === "rejected" &&
-                eosCafePromise.status === "rejected" &&
-                launchbagzTokens.status === "rejected"
-            ) {
-                return new Response("No logos found", { status: 404 });
-            }
-
-            if (alcorPromise.status === "fulfilled") {
-                const { data, error } = alcorPromise.value;
-                if (error) console.log(error);
-
-                if (data) {
-                    for (let i = 0; i < data.length; i++) {
-                        const { name, download_url } = data[i];
-
-                        const nameWithoutLastExtension = name.split(".").slice(0, -1).join(".");
-                        const [symbol, contract] = nameWithoutLastExtension.split("_");
-
-                        if (symbol && contract) {
-                            logos[`${symbol.toUpperCase()}@${contract.toLowerCase()}`] = {
-                                logo: download_url,
-                                logo_lg: download_url,
-                            };
-                        }
-                    }
-                }
-            }
-
-            if (eosCafePromise.status === "fulfilled") {
-                const { data, error } = eosCafePromise.value;
-                if (error) console.log(error);
-
-                if (data) {
-                    const filterData = data.filter((token: any) => token.chain === mainChain);
-
-                    for (let i = 0; i < filterData.length; i++) {
-                        const token = filterData[i];
-
-                        logos[`${token.symbol}@${token.account}`] = {
-                            logo: token.logo,
-                            logo_lg: token.logo_lg,
-                        };
-                    }
-                }
-            }
-
-            if (launchbagzTokens.status === "fulfilled") {
-                const { data, error } = launchbagzTokens.value;
-                if (error) console.log(error);
-
-                if (data) {
-                    for (let i = 0; i < data.length; i++) {
-                        const token = data[i];
-
-                        if (token.image) {
-                            logos[`${token.token_code.toUpperCase()}@${token.token_contract}`] = {
-                                logo: `${config.NEFTY_IPFS}/ipfs/${token.image}`,
-                                logo_lg: `${config.NEFTY_IPFS}/ipfs/${token.image}`,
-                            };
-                        }
-                    }
-                }
-            }
-
-            await env.put(
-                id,
-                JSON.stringify({
-                    // cache for 1 hour
-                    timestamp: timestamp(3600),
-                    data: logos,
-                })
-            );
+            if (launchbagzUrl) launchbagzUrl = config.NEFTY_API_TEST;
+        } else if (chain.includes("main")) {
+            mainChain = chain.replace("mainnet", "").replace("main", "");
         }
+
+        const logos = (
+            await Promise.all([
+                getAlcorLogos(env, mainChain),
+                getEosCafeLogos(env, mainChain),
+                getLaunchbagzLogos(env, chain, launchbagzUrl),
+            ])
+        ).reduce(
+            (acc, curr) => ({
+                ...acc,
+                ...curr,
+            }),
+            {}
+        );
 
         if (token) {
             if (logos[token])
